2da99e4a70b90754d39ced2ebf111344c3f81b2a
Amresh Kumar <amresh.kumar@kanerika.com-at-972218571398>
2023-09-18 14:52:00 +0530

    My first commit

---
+++ management/src/main/java/com/property/management/converter/PropertyConverter.java
@@ -0,0 +1,31 @@
+package com.property.management.converter;
+
+import com.property.management.dto.PropertyDTO;
+import com.property.management.entity.PropertyEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropertyConverter {
+    public PropertyDTO convertEntityToDTO(PropertyEntity propertyEntity) {
+        PropertyDTO propertyDTO = new PropertyDTO();
+        propertyDTO.setId(propertyEntity.getId());
+        propertyDTO.setTitle(propertyEntity.getPropertyTitle());
+        propertyDTO.setDescription(propertyEntity.getDescription());
+        propertyDTO.setPrice(propertyEntity.getPrice());
+        propertyDTO.setAddress(propertyEntity.getAddress());
+        propertyDTO.setUserId(propertyEntity.getUserEntity().getId());
+
+        return propertyDTO;
+    }
+
+    public static PropertyEntity convertDTOToEntity(PropertyDTO propertyDTO) {
+        PropertyEntity propertyEntity = new PropertyEntity();
+        propertyEntity.setPropertyTitle(propertyEntity.getPropertyTitle());
+        propertyEntity.setDescription(propertyDTO.getDescription());
+        propertyEntity.setPrice(propertyDTO.getPrice());
+        propertyEntity.setAddress(propertyDTO.getAddress());
+
+        return propertyEntity;
+    }
+
+}
---
+++ management/src/main/java/com/property/management/converter/UserConverter.java
@@ -0,0 +1,30 @@
+package com.property.management.converter;
+
+import com.property.management.dto.UserDTO;
+import com.property.management.entity.UserEntity;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserConverter {
+    public UserEntity convertDTOtoEntity(UserDTO userDTO) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setFirstName(userDTO.getFirstName());
+        userEntity.setFirstName(userDTO.getLastName());
+        userEntity.setOwnerEmail(userEntity.getOwnerEmail());
+        userEntity.setPassword(userEntity.getPassword());
+        userEntity.setMobileNumber(userEntity.getMobileNumber());
+        return userEntity;
+    }
+
+
+    public UserDTO convertEntityToDTO(UserEntity userEntity) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setFirstName(userEntity.getFirstName());
+        userDTO.setLastName(userEntity.getLastName());
+        userDTO.setEmail(userEntity.getOwnerEmail());
+        userDTO.setPassword(userEntity.getPassword());
+        userDTO.setMobileNumber(userEntity.getMobileNumber());
+        return userDTO;
+    }
+}
